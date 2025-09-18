4bd2150b641a819ed472e72e7cfc1db0aee7eb9b
devmeester <bermudes13@outlook.com>
2025-09-18 14:19:16 +0100

    coverage html

--- recipes/tests/test_recipe_base.py
+++ recipes/tests/test_recipe_base.py
@@ -1,19 +1,20 @@
 from django.test import TestCase
 from recipes.models import Category, Recipe, User
 
+
 class RecipeTestBase(TestCase):
-    def setUp(self):
+    def setUp(self) -> None:
         return super().setUp()
-    
+
     def make_category(self, name='Category'):
         return Category.objects.create(name=name)
-    
+
     def make_author(
         self,
         first_name='user',
         last_name='name',
         username='username',
-        password='12345',
+        password='123456',
         email='username@email.com',
     ):
         return User.objects.create_user(
@@ -23,28 +24,28 @@ class RecipeTestBase(TestCase):
             password=password,
             email=email,
         )
-        
+
     def make_recipe(
         self,
         category_data=None,
         author_data=None,
-        title='recipe title',
-        description= 'recipe description',
-        slug= 'recipe-title',
-        preparation_time= 10,
-        preparation_time_unit= 'minutos',
-        servings= 4,
-        servings_unit= 'pessoas', 
-        preparation_steps= 'teste',
-        preparation_steps_is_html= False,
-        is_published= True,        
+        title='Recipe Title',
+        description='Recipe Description',
+        slug='recipe-slug',
+        preparation_time=10,
+        preparation_time_unit='Minutos',
+        servings=5,
+        servings_unit='Porções',
+        preparation_steps='Recipe Preparation Steps',
+        preparation_steps_is_html=False,
+        is_published=True,
     ):
         if category_data is None:
             category_data = {}
-            
+
         if author_data is None:
             author_data = {}
-            
+
         return Recipe.objects.create(
             category=self.make_category(**category_data),
             author=self.make_author(**author_data),
@@ -54,7 +55,7 @@ class RecipeTestBase(TestCase):
             preparation_time=preparation_time,
             preparation_time_unit=preparation_time_unit,
             servings=servings,
-            servings_unit=servings_unit, 
+            servings_unit=servings_unit,
             preparation_steps=preparation_steps,
             preparation_steps_is_html=preparation_steps_is_html,
             is_published=is_published,
